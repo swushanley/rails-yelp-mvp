6720501a0e098ad8d78011a439623afe1f4e5517
Sophia Wushanley <swushanley@gmail.com>
2017-11-22 11:33:41 +0100

    added show and index pages for restaurants and reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :new]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -22,10 +22,6 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
       params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-    end
-
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,33 @@
 class ReviewsController < ApplicationController
-  def show
+  before_action :set_restaurant
+
+  def index
+    @restaurant.reviews
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "restaurants/show"
+    end
   end
+
+  def new
+    @review = Review.new
+  end
+
+  private
+
+  def set_restaurant
+     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
